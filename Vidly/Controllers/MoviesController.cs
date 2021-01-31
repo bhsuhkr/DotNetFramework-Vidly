435b9789f90142e8f963273a3552f6251ce1afc2
Bo <bhsuhkr@naver.com>
2021-01-30 20:30:10 -0600

    AntiForgeryToken

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -66,6 +66,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,6 +11,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Genre { get; set; }
