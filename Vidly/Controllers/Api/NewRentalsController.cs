8da25a875b3e51a979b622f603a7b81aaf86c196
Bo <bhsuhkr@naver.com>
2021-02-01 14:19:59 -0600

    rental

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -1,19 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private readonly ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
